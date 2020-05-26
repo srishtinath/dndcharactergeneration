2762be6cf687955491730de56e027172726cdac7
Srishti Nath <srishti.nath@gmail.com>
2020-05-26 11:52:54 -0400

    associations

---
+++ db/migrate/20200526154547_create_character_abilities.rb
@@ -0,0 +1,9 @@
+class CreateCharacterAbilities < ActiveRecord::Migration[6.0]
+  def change
+    create_table :character_abilities do |t|
+      t.integer :character_id
+      t.integer :ability_id
+      t.timestamps
+    end
+  end
+end
