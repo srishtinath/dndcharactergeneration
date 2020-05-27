3bf24ec038c7b20d49143d83828587e26c892755
Srishti Nath <srishti.nath@gmail.com>
2020-05-27 17:43:00 -0400

    break time

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,20 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :jobs, :abilities, :spells
   
-  get 'characters/new', to: "character#new", as: 'new_character'
-  get 'characters/:id/new_abilities', to: "character#new_abilities", as: 'new_abilities'
-  get 'characters/:id/new_spells', to: "character#new_spells", as: 'new_spells'
-  post 'characters', to: "character#create"
-  patch 'characters/:id/abilities', to: "character#add_abilities"
-  patch 'characters/:id/spells', to: "character#add_spells"
+  get 'characters', to: "characters#index", as: 'characters'
+
+  get 'characters/:id/new_abilities', to: "characters#new_abilities", as: 'new_abilities'
+  get 'characters/:id/new_spells', to: "characters#new_spells", as: 'new_spells'
+  post 'characters', to: "characters#create"
+  patch 'characters/:id/abilities', to: "characters#add_abilities"
+  patch 'characters/:id/spells', to: "characters#add_spells"
+  get 'characters/new', to: "characters#new", as: 'new_character'
   
-  get 'characters/:id/edit', to: "character#edit", as: 'edit_character'
-  get 'characters/:id/edit_spells', to: "character#edit_spells", as: 'edit_spells'
-  get 'characters/:id/edit_abilities', to: "character#edit_abilities", as: 'edit_abilities'
-  patch 'characters/:id', to: "character#update"
-  patch 'characters/:id/edit_abilities', to: "character#update_abilities"
-  patch 'characters/:id/edit_spells', to: "character#update_spells"
+  get 'characters/:id/edit', to: "characters#edit", as: 'edit_character'
+  get 'characters/:id/edit_spells', to: "characters#edit_spells", as: 'edit_spells'
+  get 'characters/:id/edit_abilities', to: "characters#edit_abilities", as: 'edit_abilities'
+  patch 'characters/:id', to: "characters#update"
+  patch 'characters/:id/edit_abilities', to: "characters#update_abilities"
+  patch 'characters/:id/edit_spells', to: "characters#update_spells"
 
 end
