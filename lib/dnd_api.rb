114bff1a7302a7cd94af06d24f4122af42295792
josephchilin <joseph@josephchilin.com>
2020-05-26 20:18:02 -0400

    nested spells url api works

--- lib/dnd_api.rb
+++ lib/dnd_api.rb
@@ -17,9 +17,28 @@ class DndApi
     def class_details(job)
         @request_hash = self.class.get('/#{find_class.url}').parsed_response
     end
+
+    def description(result)
+        @desc_hash = self.class.get(result).parsed_response
+        @desc_hash["desc"]
+    end
+
+    def spells
+        request_hash = self.class.get('/api/features').parsed_response
+        request_hash["results"].each do |result|
+            # byebug
+            @url = result["url"]
+            # byebug
+            @description = description(@url)
+            # byebug
+            Spell.create(name: result["name"], description: @description)
+        end
+    end
+
+
 end
 
 # api = DndApi.new 
-# byebug
-# api.classes
+# # byebug
+# api.spells
 # byebug
